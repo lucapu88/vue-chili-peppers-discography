68ac61480e099ed3eaa8b66e3d21ad145d71caa9
lucapu88 <caputoluca88@gmail.com>
2020-04-20 17:37:00 +0200

    test

---
+++ resources/js/app.js
@@ -0,0 +1,20 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+import "./bootstrap";
+
+import Vue from "vue";
+import Routes from "./routes.js";
+
+import App from "./views/App";
+
+const app = new Vue({
+    el: "#app",
+    router: Routes,
+    render: h => h(App)
+});
+
+export default app;
